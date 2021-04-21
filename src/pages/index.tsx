9587e3604bdc126bf96d7cdda0e0a7514dd7a360
Felipe Lobo <felipeblobo@gmail.com>
2021-04-20 21:58:55 -0300

    ajustando logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { Header } from "../components/Header";
 export default function Home(props) {
   return (
   <>
-   <h1>Index</h1>
    {/* <p>{JSON.stringify(props.episodes)}</p> */}
   </>
   )
