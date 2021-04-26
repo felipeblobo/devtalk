78f91becd343bd90dc2357702cfa06dd9f5089ab
Felipe Lobo <felipeblobo@gmail.com>
2021-04-26 11:10:57 -0300

    adequando viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
      return (
      <PlayerContextProvider>
         <head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </head>
         <div className ={styles.wrapper}>
           <main>
